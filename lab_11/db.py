1bf93021e9349d4fa0b53cda9d34b82c96a14b50
Doszhan <dos003034@gmail.com>
2024-05-04 15:19:56 +0500

    'update'

--- lab_11/db.py
+++ lab_11/db.py
@@ -61,4 +61,44 @@ def delete_data(name=None, phone=None):
         cur.execute(f"""DELETE FROM phonebook WHERE phone = '{phone}' """)
     conn.commit()
 
+def get_records_by_pattern(pattern):
+    cur.execute(f"""SELECT * FROM phonebook WHERE name LIKE '{pattern}'
+     OR last_name LIKE '{pattern}'
+     OR phone LIKE '{pattern}'
+                """)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
+def insert_or_update_user(name, last_name, phone):
+    cur.execute(f"""SELECT * FROM phonebook WHERE first_name = '{name}'
+     AND last_name = '{last_name}';
+                """)
+    if cur.fetchone() is None:
+        cur.execute(f"""INSERT INTO phonebook (first_name, last_name, phone) VALUES "
+        ('{name}', '{last_name}', '{phone}')
+                    """)
+    else:
+        cur.execute(f"""UPDATE phonebook SET phone = '{phone}' WHERE name = '{name}' AND last_name = '{last_name}'""")
+    conn.commit()
+
+def insert_many_users(user_list):
+    for user in user_list:
+        name, last_name, phone = user
+        if len(phone) == 12:
+            insert_or_update_user(name, last_name, phone)
+        else:
+            print(f"Incorrect data: {user}")
+
+def query_data_with_pagination(limit, offset):
+    cur.execute("SELECT * FROM phonebook LIMIT %s OFFSET %s", (limit, offset))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def delete_data_by_username_or_phone(username=None, phone=None):
+    if username:
+        cur.execute(f"""DELETE FROM phonebook WHERE name = '{username}' OR last_name = '{username}' """)
+    if phone:
+        cur.execute(f"""DELETE FROM phonebook WHERE phone = '{phone}' """)
+    conn.commit()
